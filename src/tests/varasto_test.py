c7cf5a3b42285152eb9b1795a5baed53526a2d24
Iida Vilkkinen <iida.vilkkinen@gmail.com>
2021-11-05 02:47:00 +0200

    .gitignore modified, varasto_test.py modified

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,49 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_vaaranlainen_tilavuus_korjaantuu_oikein(self):
+        self.varasto2 = Varasto(-1)
+
+        self.assertEqual(self.varasto2.tilavuus, 0.0)
+    
+    def test_vaaranlainen_saldo_korjaantuu_oikein(self):
+        self.varasto3 = Varasto(10,-1)
+
+        self.assertEqual(self.varasto3.saldo, 0.0)
+
+    def test_alku_saldo_on_isompi_kuin_tilavuus(self):
+        self.varasto4 = Varasto(10,20)
+
+        self.assertEqual(self.varasto4.saldo,10)
+
+    def test_varastoon_lisataan_maara_joka_on_alle_0(self):
+        self.varasto.lisaa_varastoon(-1)
+        
+        self.assertEqual(self.varasto.saldo, 0.0)
+
+    def test_varastoon_lisataan_enemman_kuin_mahtuu(self):
+        self.varasto.lisaa_varastoon(11)
+
+        self.assertEqual(self.varasto.saldo, 10)
+    
+    def test_varastosta_otetaan_vahemman_kuin_0(self):
+        self.varasto.lisaa_varastoon(5)
+
+        self.assertEqual(self.varasto.ota_varastosta(-1), 0.0)
+    
+    def test_varastosta_otetaan_enemman_kuin_saldoa(self):
+        self.varasto.lisaa_varastoon(5)
+        
+        self.assertEqual(self.varasto.ota_varastosta(7), 5)
+    
+    def test_varastosta_otetaan_enemmän_kuin_saldoa_saldo_oikein(self):
+        self.varasto.lisaa_varastoon(5)
+        self.varasto.ota_varastosta(7)
+
+        self.assertEqual(self.varasto.saldo, 0.0)
+    
+    def test_str_funktio_tulostaa_oikein(self):
+        self.varasto.lisaa_varastoon(6)
+        
+        self.assertEqual(self.varasto.__str__(), "saldo = 6, vielä tilaa 4")
